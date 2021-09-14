5080e5c0292e57dccd50b6c149d40571db5a9f72
bobacer <musgravehill@gmail.com>
2021-09-14 21:23:02 +0300

    ok

--- SRC/sonarDepth/sonarDepth.ino
+++ SRC/sonarDepth/sonarDepth.ino
@@ -5,8 +5,10 @@ volatile uint32_t SONAR_timeAllowListen_mks = 1L; //time to next listen sync aft
 volatile uint32_t SONAR_pulseStart_mks = 1L; //time the pulse started. Used in calculation of the pulse length
 volatile uint8_t SONAR_state = 1; //1=sync process 2=depth process
 volatile uint32_t SONAR_pulseDepthLength_mks = 1; //mks for sonar depth
-#define SONAR_allowNextSync_mks 244450  //min time to get new sync-pulse  (sonar send data 3-4Hz)
+#define SONAR_allowNextSync_mks 230832  //min time to get new sync-pulse  (sonar send data 3-4Hz)
 #define SONAR_depthMax_mks 59555
+
+volatile uint32_t SONAR_tmp_mks = 1;
 
 //================================== TIMEMACHINE =================================================================
 uint32_t TIMEMACHINE_next_311ms = 0L;
@@ -18,34 +20,38 @@ void setup() {
 }
 
 void loop() {
-TIMEMACHINE_loop();
+  TIMEMACHINE_loop();
 }
 
 void SONAR_ISR() {
-  if (SONAR_state == 1 && SONAR_timeAllowListen_mks < micros()) {
+  uint32_t mcrs = micros();
+  if (SONAR_state == 1 && SONAR_timeAllowListen_mks < mcrs) {
     return;  //waiting for Sync, but time too less from prev syncOk
   }
 
   if (SONAR_state == 1) { //sync
     if (digitalRead(SONAR_pin) == HIGH) { //sync time start at rising
-      SONAR_pulseStart_mks = micros();
+      SONAR_pulseStart_mks = mcrs;
     }
     else { //sync time finish at falling
-      uint32_t delta_mks = micros() - SONAR_pulseStart_mks;
+      uint32_t delta_mks = mcrs - SONAR_pulseStart_mks;
       if (delta_mks > 6660 && delta_mks < 6700) {
         SONAR_state = 2;
-        SONAR_timeAllowListen_mks = micros() + SONAR_allowNextSync_mks;
-        SONAR_pulseStart_mks = micros();
+        SONAR_timeAllowListen_mks = mcrs + SONAR_allowNextSync_mks;
+        SONAR_pulseStart_mks = mcrs;
+        SONAR_tmp_mks = mcrs;
       }
     }
   }
 
   if (SONAR_state == 2) { //depth
     if (digitalRead(SONAR_pin) == HIGH) {
-      uint32_t delta_mks = micros() - SONAR_pulseStart_mks;
+      uint32_t delta_mks = mcrs - SONAR_pulseStart_mks;
       SONAR_state = 1;
       if (delta_mks > 800 && delta_mks < SONAR_depthMax_mks) {
         SONAR_pulseDepthLength_mks = delta_mks;
+      } else {
+        SONAR_pulseDepthLength_mks = 1;
       }
     }
   }
