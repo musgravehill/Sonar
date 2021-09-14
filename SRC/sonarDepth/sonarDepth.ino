2c94d6aec38314d7f18839877777c1bf3aafbedf
bobacer <musgravehill@gmail.com>
2021-09-14 21:51:28 +0300

    man

--- SRC/sonarDepth/sonarDepth.ino
+++ SRC/sonarDepth/sonarDepth.ino
@@ -6,13 +6,14 @@ volatile uint32_t SONAR_pulseStart_mks = 1L; //time the pulse started. Used in c
 volatile uint8_t SONAR_state = 1; //1=sync process 2=depth process
 volatile uint32_t SONAR_pulseDepthLength_mks = 1; //mks for sonar depth
 #define SONAR_allowNextSync_mks 244450  //min time to get new sync-pulse  (sonar send data 3-4Hz)
-#define SONAR_depthMax_mks 59555
+#define SONAR_depthMax_mks 58111
 
 //================================== TIMEMACHINE =================================================================
 uint32_t TIMEMACHINE_next_311ms = 0L;
 
 
 void setup() {
+  DDRD &= ~(1 << PD2); //set d2 input
   attachInterrupt(0, SONAR_ISR, CHANGE);
   Serial.begin(57600);
 }
@@ -28,15 +29,15 @@ void SONAR_ISR() {
   }
 
   if (SONAR_state == 1) { //sync
-    if (digitalRead(SONAR_pin) == HIGH) { //sync time start at rising
+    if (PIND & (1 << PD2)) { //sync time start at rising => d2 is high
       SONAR_pulseStart_mks = mcrs;
     }
     else { //sync time finish at falling
       uint32_t delta_mks = mcrs - SONAR_pulseStart_mks;
       if (delta_mks > 6660 && delta_mks < 6700) {
         SONAR_state = 2;
-        SONAR_timeAllowListen_mks = mcrs + SONAR_allowNextSync_mks;        
-        SONAR_pulseStart_mks = mcrs;       
+        SONAR_timeAllowListen_mks = mcrs + SONAR_allowNextSync_mks;
+        SONAR_pulseStart_mks = mcrs;
       }
     }
   }
