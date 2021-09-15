a758c438c24efd08f13619b2a709340108ead753
bobacer <musgravehill@gmail.com>
2021-09-15 23:31:36 +0300

    v2

--- SRC/SonarLoggerTinyGps/MONITOR.ino
+++ SRC/SonarLoggerTinyGps/MONITOR.ino
@@ -6,9 +6,16 @@ void MONITOR_init() {
 void MONITOR_render() {
   lcd.clear();
   lcd.setCursor(0, 0);
-  //lcd.print(GPS_string.substring(0, 16));
+
+  float flat, flon;
+  unsigned long age;
+  gps.f_get_position(&flat, &flon, &age);
+  lcd.print(flat, 6);
+  lcd.print(' ');
+  lcd.print(flon, 6);
+
   lcd.setCursor(0, 1);
-  lcd.print((SONAR_getDepth_cm()/100.0), 2);
+  lcd.print((SONAR_getDepth_cm() / 100.0), 2);
   if (SD_isError) {
     lcd.setCursor(8, 1);
     lcd.print(F("SD_ERR"));
--- SRC/SonarLoggerTinyGps/SD.ino
+++ SRC/SonarLoggerTinyGps/SD.ino
@@ -1,13 +1,22 @@
 void SD_logData() {
   if (SYS_GPS_isNewData) {
-    uint16_t depth = SONAR_getDepth_cm();
-    myFile = sd.open(SYS_LOG_FileName, FILE_WRITE);
-    if (myFile) {
-      myFile.println(depth);
-      myFile.close();
-      SYS_GPS_isNewData = false;
-    }else {
-      SD_isError = true;
+    float flat, flon;
+    unsigned long age;
+    gps.f_get_position(&flat, &flon, &age);
+    if (flat != TinyGPS::GPS_INVALID_F_ANGLE && flon != TinyGPS::GPS_INVALID_F_ANGLE) {
+      uint16_t depth = SONAR_getDepth_cm();
+      myFile = sd.open(SYS_LOG_FileName, FILE_WRITE);
+      if (myFile) {
+        myFile.print(flat, 6);
+        myFile.print(';');
+        myFile.print(flon, 6);
+        myFile.print(';');
+        myFile.println(depth);
+        myFile.close();
+        SYS_GPS_isNewData = false;
+      } else {
+        SD_isError = true;
+      }
     }
   }
 }
