4b3b8040741d6ce47a9374bf039c9e3f184e4f7d
musgravehill <musgravehill@gmail.com>
2021-09-18 22:09:45 +0300

    sd

--- SRC/SonarLogicLogger/SD.ino
+++ SRC/SonarLogicLogger/SD.ino
@@ -2,14 +2,23 @@ void SD_logData_continuously() {
   if (!SONAR_isProcessTodo) {
     return;
   }
-  SONAR_isProcessTodo = false;
   myFile = sd.open(SYS_LOG_FileName, FILE_WRITE);
-  if (myFile) {    
-    myFile.close();     
+  if (myFile) {
+    myFile.print(F("R;"));
+    for (byte i = 0; i <= SONAR_pulses_rising_idx_max; i++) {
+      myFile.print(SONAR_pulses_rising[i], DEC);
+    }
+    myFile.print(F("F;"));
+    for (byte i = 0; i <= SONAR_pulses_falling_idx_max; i++) {
+      myFile.print(SONAR_pulses_falling[i], DEC);
+    }
+    myFile.println();
+    myFile.close();
     SD_records_count++;
   } else {
     SD_isError = true;
   }
+  SONAR_isProcessTodo = false;
 }
 
 void SD_init() {
